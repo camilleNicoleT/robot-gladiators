6371e4a2efb448ea1f0e59bbca91e8f588ef361a
camilleNicole <camilleht@gmail.com>
2021-12-28 15:02:24 -0500

    Skip Bug Fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,29 +6,44 @@ var randomNumber = function(min,max) {
       
   return value;
 };
-
-  //fight function
-var fight = function(enemy) {
-    while (playerInfo.health > 0 && enemy.health > 0) {
+var fightOrSkip = function() {
+    //ask player if they'd like to fight or skip using fightOrSkip function
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-      
-    if (promptFight === "skip" || promptFight === "SKIP") {
+       
+    // Conditional Recursive Function Call
+    if (promptFight === "" || promptFight === null) {
+     window.alert("You need to provide a valid answer! Please try again.");
+     return fightOrSkip(); }
+
+     promptFight = promptFight.toLowerCase();
+
+     if (promptFight === "skip") {
          // confirm player wants to skip
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-          
-         // if yes (true), leave fight
+    
+    // if yes (true), leave fight
       if (confirmSkip) {
          window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
         // subtract money from playerInfo.money for skipping
-        playerInfo.money = playerInfo.money - 10;
-        console.log("playerInfo.money", playerInfo.money);
-         break;
-      }
-    } 
-    // generate random damage value based on player's attack power
-     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+        playerInfo.money = Math.max(0, playerInfo.money - 10);
 
-     enemy.health = Math.max(0, enemy.health - damage);
+         // return true if player wants to leave
+      return true;
+
+      }
+    }
+    return false;
+}
+  //fight function
+var fight = function(enemy) {
+    while (playerInfo.health > 0 && enemy.health > 0) {
+       if (fightOrSkip()) {
+           break;
+       } 
+    
+       var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+      
+      enemy.health = Math.max(0, enemy.health - damage);
      console.log(
        playerInfo.name + ' attacked ' + enemy.name + '. ' + enemy.name + ' now has ' + enemy.health + ' health remaining.'
      );
